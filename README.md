1afea9a1ee410008961d6764c82c17a399a06094
Corey A <hawaiideveloper@gmail.com>
2024-11-06 20:23:00 -0500

    README.md updated for worker nodes to join control plane

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # k8-provisions
+
+
+The sample below shows how to connect / join worker nodes to the control
+plane.
+
+
+![example_1.png]
+
+
+![example_2.png]
--- worker-nodes-only.sh
+++ worker-nodes-only.sh
@@ -9,9 +9,6 @@
 # Notes: Additional details, if any
 # This will require the token generated from control plane
 
-![example_1.png]
 
-
-![example_2.png]
 echo "please see token generated using the below comand on the control plane server"
 echo "kubeadm token create --print-join-command"
