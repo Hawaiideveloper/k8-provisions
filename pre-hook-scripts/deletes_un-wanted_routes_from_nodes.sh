ff68a52201fea2a1af5845e42dcb7fac8a6060d8
Corey A <hawaiideveloper@gmail.com>
2024-12-08 05:08:26 -0500

    deleted un-needed routes

---
+++ pre-hook-scripts/deletes_un-wanted_routes_from_nodes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+```bash
+sudo ip route del default via 192.168.69.1 dev ens36
+sudo ip route del default via 192.168.79.1 dev ens35
+```
+
+
+
+echo "The script removes the routes / GW from nodepor and podnete"
+
+
+
+
